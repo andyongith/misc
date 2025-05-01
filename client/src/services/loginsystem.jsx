414da899fb02df093a28968f660fc9627b32e854
5ovit <5ovit@users.noreply.github.com>
2025-05-01 18:01:17 +0530

    Implemented login page

--- client/src/services/loginsystem.jsx
+++ client/src/services/loginsystem.jsx
@@ -1,6 +1,15 @@
 function handleLogin(data) {
-    console.log("Handling your login");
-    console.log(data);
+    fetch(`${__API_URL__}/api/v1/auth/login`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            'identifier': data.username,
+            'password': data.password
+        })
+    }).then(res => res.json())
+      .then(data => console.log(data));
 }
 
 function handleSignup(data) {
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd())
   return {
@@ -10,6 +9,9 @@ export default defineConfig(({mode}) => {
       react(),
       tailwindcss(),
     ],
-    base: env.VITE_BASE_URL || "/"
+    base: env.VITE_BASE_URL || "/",
+    define: {
+      __API_URL__: JSON.stringify(env.VITE_API_URL || "")
+    }
   }
 });
