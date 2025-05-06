f98ed060f4a707e82055abd84838a4c7a9778303
5ovit <5ovit@users.noreply.github.com>
2025-05-06 16:04:51 +0530

    Tweaked Header and PageBase a little

--- client/src/components/PageBase.jsx
+++ client/src/components/PageBase.jsx
@@ -11,11 +11,11 @@ export default function PageBase({ children, className = "", ...props }) {
 
     return <div className="
     flex flex-col w-full min-h-screen h-full bg-light dark:bg-dark/85
-    text-dark dark:text-light
+    text-dark dark:text-light items-center pl-20 pt-20
     ">
         <Header className="" />
         <SideNavigationBar />
-        <div className={"p-4 ml-25 mr-4 " + className} {...props}>
+        <div className={"p-4 " + className} {...props}>
             {children}
         </div>
     </div>
